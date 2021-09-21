24842b1ed723563bc04c9a69481c51f8fa7f8688
geun25 <sas0898@naver.com>
2021-09-21 22:17:52 +0900

    .

---
+++ DataBase_prac/AspNetWindow/ApplicationRoot.cs
@@ -0,0 +1,15 @@
+﻿using AppConfiguration;
+using System.Windows.Forms;
+
+namespace AdoNetWindow
+{
+    public partial class ApplicationRoot : Form
+    {
+        protected ConfigurationMgr instance;
+        public ApplicationRoot()
+        {
+            InitializeComponent();
+            instance = ConfigurationMgr.Instance();
+        }
+    }
+}
--- DataBase_prac/AspNetWindow/Form1.cs
+++ DataBase_prac/AspNetWindow/Form1.cs
@@ -1,15 +1,17 @@
-﻿using AdoNet;
-using AppConfiguration;
+﻿using AdoNetWindow.Model;
 using System;
-using System.Configuration;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
-namespace AspNetWindow
-{ 
-    public partial class Form1 : Form
+namespace AdoNetWindow
+{
+    public partial class Form1 : ApplicationRoot
     {
-        MsSql msql;
-        string ConnectionString;
+
+        //MsSql msql;
+        //string ConnectionString;
         public Form1()
         {
             InitializeComponent();
@@ -17,17 +19,39 @@ namespace AspNetWindow
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            ReadConfiguration();
+
         }
 
         private void ReadConfiguration()
         {
-            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-            ConnectionString = config.ConnectionStrings.ConnectionStrings["WinConnection"].ToString();
-            msql = new MsSql(ConnectionString);
-            ConfigurationMgr instance = ConfigurationMgr.Instance();
-            MessageBox.Show(instance.Connection.State.ToString());
-            instance.Connection.Close();
+            //Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            //ConnectionString = config.ConnectionStrings.ConnectionStrings["WinConnection"].ToString();
+            //msql = new MsSql(ConnectionString);
+            //ConfigurationMgr instance = ConfigurationMgr.Instance();
+            //MessageBox.Show(instance.Connection.State.ToString());
+            //instance.Connection.Close();
         }
-    }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //SqlDataReader
+            string sql = "select * from tb_student";
+            SqlCommand command = new SqlCommand(sql, (SqlConnection)instance.Connection);
+            SqlDataReader rd = command.ExecuteReader(CommandBehavior.CloseConnection);
+            List<StudentModel> students = new List<StudentModel>();
+            while(rd.Read())
+                students.Add(GetStudentModel(rd));
+            foreach(StudentModel model in students)
+                Console.WriteLine(model.ToString());
+        }
+
+        private StudentModel GetStudentModel(SqlDataReader rd)
+        {
+            StudentModel model = new StudentModel();
+            model.StudentId = int.Parse(rd.GetString(1).ToString(); 
+            model.StudentName = rd["StudentName"].ToString();
+            model.Address = rd["Address"].ToString();
+            return model;
+        }
+    } 
 }
